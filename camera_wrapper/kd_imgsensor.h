e5eafa2cd9100ce0e0fef391dce08f5f972ec4e9
Rohan Taneja <rohantaneja63@gmail.com>
2017-05-01 02:04:57 +0530

    camera_wrapper: update kd_*.h from stock

--- camera_wrapper/kd_imgsensor.h
+++ camera_wrapper/kd_imgsensor.h
@@ -2,9 +2,9 @@
 #define _KD_IMGSENSOR_H
 
 #include <linux/ioctl.h>
-//#define CONFIG_COMPAT
+/* #define CONFIG_COMPAT */
 #ifdef CONFIG_COMPAT
-//64 bit
+/* 64 bit */
 #include <linux/fs.h>
 #include <linux/compat.h>
 #endif
@@ -26,25 +26,25 @@
 *
 ********************************************************************************/
 #define YUV_INFO(_id, name, getCalData)\
-    { \
-	_id, name, \
-	NSFeature::YUVSensorInfo <_id>::createInstance(name, #name), \
-		(NSFeature::SensorInfoBase*(*)()) \
-	NSFeature::YUVSensorInfo <_id>::getInstance, \
-	NSFeature::YUVSensorInfo <_id>::getDefaultData, \
+	{ \
+		_id, name, \
+NSFeature :  : YUVSensorInfo < _id >  :  : createInstance(name, #name), \
+		(NSFeature :  : SensorInfoBase*(*)()) \
+NSFeature :  : YUVSensorInfo < _id >  :  : getInstance, \
+NSFeature :  : YUVSensorInfo < _id >  :  : getDefaultData, \
 		getCalData, \
-	NSFeature::YUVSensorInfo <_id>::getNullFlickerPara \
-    }
+NSFeature :  : YUVSensorInfo < _id >  :  : getNullFlickerPara \
+	}
 #define RAW_INFO(_id, name, getCalData)\
-    { \
-	_id, name, \
-	NSFeature::RAWSensorInfo <_id>::createInstance(name, #name), \
-		(NSFeature::SensorInfoBase*(*)()) \
-	NSFeature::RAWSensorInfo <_id>::getInstance, \
-	NSFeature::RAWSensorInfo <_id>::getDefaultData, \
+	{ \
+		_id, name, \
+NSFeature :  : RAWSensorInfo < _id >  :  : createInstance(name, #name), \
+		(NSFeature :  : SensorInfoBase*(*)()) \
+NSFeature :  : RAWSensorInfo < _id >  :  : getInstance, \
+NSFeature :  : RAWSensorInfo < _id >  :  : getDefaultData, \
 		getCalData, \
-	NSFeature::RAWSensorInfo <_id>::getFlickerPara \
-    }
+NSFeature :  : RAWSensorInfo < _id >  :  : getFlickerPara \
+	}
 /*******************************************************************************
 *
 ********************************************************************************/
@@ -68,22 +68,22 @@
 /* set sensor driver */
 #define KDIMGSENSORIOC_X_SET_DRIVER                 _IOWR(IMGSENSORMAGIC, 35, SENSOR_DRIVER_INDEX_STRUCT)
 /* get socket postion */
-#define KDIMGSENSORIOC_X_GET_SOCKET_POS             _IOWR(IMGSENSORMAGIC, 40, uint32_t)
+#define KDIMGSENSORIOC_X_GET_SOCKET_POS             _IOWR(IMGSENSORMAGIC, 40, u32)
 /* set I2C bus */
-#define KDIMGSENSORIOC_X_SET_I2CBUS                 _IOWR(IMGSENSORMAGIC, 45, uint32_t)
+#define KDIMGSENSORIOC_X_SET_I2CBUS                 _IOWR(IMGSENSORMAGIC, 45, u32)
 /* set I2C bus */
 #define KDIMGSENSORIOC_X_RELEASE_I2C_TRIGGER_LOCK   _IO(IMGSENSORMAGIC, 50)
 /* Set Shutter Gain Wait Done */
-#define KDIMGSENSORIOC_X_SET_SHUTTER_GAIN_WAIT_DONE _IOWR(IMGSENSORMAGIC, 55, uint32_t)
+#define KDIMGSENSORIOC_X_SET_SHUTTER_GAIN_WAIT_DONE _IOWR(IMGSENSORMAGIC, 55, u32)
 /* set mclk */
 #define KDIMGSENSORIOC_X_SET_MCLK_PLL               _IOWR(IMGSENSORMAGIC, 60, ACDK_SENSOR_MCLK_STRUCT)
 #define KDIMGSENSORIOC_X_GETINFO2                   _IOWR(IMGSENSORMAGIC, 65, IMAGESENSOR_GETINFO_STRUCT)
 /* set open/close sensor index */
-#define KDIMGSENSORIOC_X_SET_CURRENT_SENSOR         _IOWR(IMGSENSORMAGIC, 70, uint32_t)
+#define KDIMGSENSORIOC_X_SET_CURRENT_SENSOR         _IOWR(IMGSENSORMAGIC, 70, u32)
 /* set GPIO */
-#define KDIMGSENSORIOC_X_SET_GPIO                   _IOWR(IMGSENSORMAGIC, 75,IMGSENSOR_GPIO_STRUCT)
+#define KDIMGSENSORIOC_X_SET_GPIO                   _IOWR(IMGSENSORMAGIC, 75, IMGSENSOR_GPIO_STRUCT)
 /* Get ISP CLK */
-#define KDIMGSENSORIOC_X_GET_ISP_CLK                _IOWR(IMGSENSORMAGIC, 80,uint32_t)
+#define KDIMGSENSORIOC_X_GET_ISP_CLK                _IOWR(IMGSENSORMAGIC, 80, u32)
 
 #ifdef CONFIG_COMPAT
 #define COMPAT_KDIMGSENSORIOC_X_GETINFO            _IOWR(IMGSENSORMAGIC, 5, COMPAT_ACDK_SENSOR_GETINFO_STRUCT)
@@ -177,6 +177,7 @@
 #define S5K5CAGX_SENSOR_ID                      0x05ca
 #define S5K8AAYX_MIPI_SENSOR_ID                 0x08aa
 #define S5K8AAYX_SENSOR_ID                      0x08aa
+/*HI*/
 #define HI841_SENSOR_ID                         0x0841
 #define HI707_SENSOR_ID                         0x00b8
 #define HI704_SENSOR_ID                         0x0096
